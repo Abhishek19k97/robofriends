295765a0e096d2964160f69ca5046ec73f600223
Abhishek Singh <imabhi19k97@gmail.com>
2020-08-15 15:31:15 +0530

    Updates

---
+++ precache-manifest.af2a5f88d1425ffc9c894338f33e43d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7f33a0496c36426207e0157cffaa1c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d0ef1417263522cb0860",
+    "url": "/robofriends/static/css/main.e362acbd.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ef1417263522cb0860",
+    "url": "/robofriends/static/js/main.9d1f64a3.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
